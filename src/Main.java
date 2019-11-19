87aae3f542cea6abd95f3ef5d75075f1f24121b1
Jasmine Wagner <jasminewagner@hotmail.com>
2019-11-19 14:10:09 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
-
-        System.out.println("aduifgaduigfasuigfiasgufa");
+        
 
     }
 }
