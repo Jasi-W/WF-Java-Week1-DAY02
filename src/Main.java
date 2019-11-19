e51dd931a2b12107c750a3d0ae89b9f790d362e5
Alex <codefactory2222@hotmail.com>
2019-11-19 13:57:48 +0100

    Testfsfv

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
 
-        System.out.println("aduifgaduigfasuigfiasgufa");
+        System.out.println("fsdvsdsvsdvvsdvsdvsdv");
+        System.out.println("NEU");
+
+
 
     }
 }
