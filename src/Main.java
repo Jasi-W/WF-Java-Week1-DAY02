3523f10c7c7f3e53c3d49a4753c5609390ebcace
Alex <codefactory2222@hotmail.com>
2019-11-19 11:27:14 +0100

    Change Test

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        System.out.println("sdfsdfdsfsdfsdf");
 
         System.out.println("TEST");
 
