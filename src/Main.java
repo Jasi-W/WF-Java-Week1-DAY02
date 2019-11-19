86f0bad12270f04c2b0b0606853f582a3a7c1032
Jasmine Wagner <jasminewagner@hotmail.com>
2019-11-19 11:27:45 +0100

    fasdfsdf

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
+        int num = 9;
         System.out.println("hi");
 
         System.out.println("TEST");
 
     }
-
-
-
 
 }
